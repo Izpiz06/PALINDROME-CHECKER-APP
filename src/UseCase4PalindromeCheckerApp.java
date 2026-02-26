e41c88e6cecdf88256bf1898b1016bb0aeba74d4
Izpiz06 <mdizaan1192@gmail.com>
2026-02-26 19:33:15 +0530

    UC4: Arr based

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Declare and initialize the input string
+        String input = "radar";
+
+        // Convert string to character array
+        char[] chars = input.toCharArray();
+
+        // Initialize pointers
+        int start = 0;
+        int end = chars.length - 1;
+
+        // Assume palindrome initially
+        boolean isPalindrome = true;
+
+        // Continue comparison until pointers cross
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
