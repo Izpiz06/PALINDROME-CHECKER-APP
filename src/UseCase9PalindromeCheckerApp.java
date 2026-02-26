94285fe54fd8ac4b354fd328f7003825b1929032
Izpiz06 <mdizaan1192@gmail.com>
2026-02-26 20:05:56 +0530

    Recursive palindrome validation

---
+++ src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+public class UseCase9PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean isPalindrome = check(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    // Recursive method to check palindrome
+    private static boolean check(String s, int start, int end) {
+
+        // Base case: If pointers cross or meet
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters do not match
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call moving inward
+        return check(s, start + 1, end - 1);
+    }
+}
